34b6db8467d2ff4b153fe3623696e3ba44de1520
Ryan Engle <rtengle@bu.edu>
2025-05-05 13:06:56 -0400

    Added some stuff to solver_loop and implemented automated testing framework

--- src/solver_loop.py
+++ src/solver_loop.py
@@ -3,7 +3,7 @@ import os
 from mpi4py import MPI
 from dolfinx.io import XDMFFile, gmshio
 from dolfinx.plot import vtk_mesh
-from dolfinx import fem, default_scalar_type, default_real_type, mesh, log
+from dolfinx import fem, default_scalar_type, default_real_type, mesh
 from basix.ufl import element, mixed_element
 import gmsh
 import pyvista
